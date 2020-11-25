00ea299cba424c85857fe2aecae0f5f89b6806ea
BjBodner <benjybo7@gmail.com>
2020-11-25 19:59:21 +0200

    trying to make a single model script

--- src/training/train_single_model.py
+++ src/training/train_single_model.py
@@ -22,7 +22,7 @@ from src.utils.cutout import Cutout
 from src.gutout.gutout_utils import BatchGradCam, get_gutout_samples, gutout_images
 from src.models.resnet import resnet18
 from src.utils.data_utils import get_dataloaders
-from src.training.training_utils import get_args, train, test, get_optimizer_and_schedular, get_csv_logger
+from src.training.training_utils import get_args, get_optimizer_and_schedular, get_csv_logger, get_model, create_experiment_dir, run_epoch
 
 
 
@@ -30,16 +30,19 @@ from src.training.training_utils import get_args, train, test, get_optimizer_and
 
 if __name__ == "__main__":
 
+    # parse arguments
     args, max_num_batches = get_args()
-    train_loader, test_loader = get_dataloaders(args)
-    model = get_model(args)
 
+    # create train and test dataloaders
+    train_loader, test_loader = get_dataloaders(args)
+
+    # create model and optimizer
+    model = get_model(args)
     optimizer, scheduler = get_optimizer_and_schedular(model, args)
 
+    # create experiment dir, csv logg and criterion
     experiment_dir, experiment_id = create_experiment_dir(args)
-
     csv_logger = get_csv_logger(experiment_dir, experiment_id, model_flag="a")
-
     criterion = nn.CrossEntropyLoss()
 
     # cast to gpu if needed
@@ -47,12 +50,15 @@ if __name__ == "__main__":
         model = model.cuda()
         criterion.cuda()
 
+    # for a single model, the gutout model is the model that is being trained
     gutout_model = model
     training_model = model
     best_acc = -1
 
+    # create a gra
     grad_cam = BatchGradCam(model=gutout_model, feature_module=getattr(gutout_model, args.feature_module),
                             target_layer_names=[args.target_layer_names], use_cuda=args.use_cuda)
 
+    # run the training loop on a single model
     for epoch in range(args.epochs):
         best_acc = run_epoch(training_model, grad_cam, criterion, optimizer, scheduler, csv_logger, train_loader, test_loader, epoch, best_acc, max_num_batches, experiment_dir, experiment_id, args, model_flag="a")
