de356376bee6c891a248b510d3f06644d6a0a103
BjBodner <benjybo7@gmail.com>
2020-12-07 07:06:27 +0200

    edited visualization files

--- src/visualization/generate_plots_single_run.py
+++ src/visualization/generate_plots_single_run.py
@@ -3,3 +3,20 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from visualization_utils import load_csv_into_dataframe, generate_accuracy_plot, generate_gradcam_amp_plot, generate_pct_gutout_pixels_plot, generate_pct_gutout_pixels_n_gradamp_plot
+
+
+
+if __name__ == "__main__":
+
+    csv_filename = (
+        r"C:\Users\benjy\Desktop\DL_results\single_model_gutout_thresh_09\cifar10_cutout_resnet18_a.csv"
+    )
+    experiment_string = "model_a"
+    save_dir = r"results\cifar10\single_run"
+    df = load_csv_into_dataframe(csv_filename)
+
+
+    generate_accuracy_plot(df, experiment_string, save_dir)
+    generate_gradcam_amp_plot(df, experiment_string, save_dir)
+    generate_pct_gutout_pixels_plot(df, experiment_string, save_dir)
+    generate_pct_gutout_pixels_n_gradamp_plot(df, experiment_string, save_dir)
--- src/visualization/visualization_utils.py
+++ src/visualization/visualization_utils.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
 
 def load_csv_into_dataframe(csv_filename):
@@ -33,32 +34,38 @@ def generate_accuracy_plot(df, experiment_string, save_dir):
     plt.legend(["train accuracy", "test accuracy"])
     plt.xlabel("epochs")
     plt.ylabel("accuracy")
-    plt.title(f"accuracy_{experiment_string}")
+    plt.title(f"accuracy - {experiment_string}")
     plt.savefig(os.path.join(save_dir, f"accuracy_{experiment_string}.png"))
+    plt.close()
 
 
 def generate_gradcam_amp_plot(df, experiment_string, save_dir):
     os.makedirs(save_dir, exist_ok=True)
 
-    plt.errorbar(df["epoch"], df["gradamp_mean_mean"], yerr=df["gradamp_std_mean"], fmt='-o')
+    plt.errorbar(df["epoch"], df["gradamp_mean_mean"], yerr=[np.zeros(len(df["gutout_std_mean"])), df["gutout_std_mean"]/1024.0], fmt='-o')
 
     plt.legend(["mean amplitude of gradcam"])
     plt.xlabel("epochs")
     plt.ylabel("amplitude of gradcam")
-    plt.title(f"Mean amplitude of gradcam w error {experiment_string}")
-    plt.savefig(os.path.join(save_dir, f"gradamp_w_error_{experiment_string}.png"))
+    plt.title(f"Amplitude of gradcam - {experiment_string}")
+    plt.savefig(os.path.join(save_dir, f"gradamp_amp_{experiment_string}.png"))
+    plt.close()
 
 
 def generate_pct_gutout_pixels_plot(df, experiment_string, save_dir):
     os.makedirs(save_dir, exist_ok=True)
 
-    plt.errorbar(df["epoch"], df["train_num_masked_pixel"]/1024.0, yerr=df["gutout_std_mean"]/1024.0, fmt='-o')
+    # plt.errorbar(df["epoch"], df["train_num_masked_pixel"]/1024.0, yerr=[np.zeros(len(df["gutout_std_mean"])), df["gutout_std_mean"]/1024.0], fmt='-o')
+    # plt.plot(df["epoch"], df["train_num_masked_pixel"]/1024.0, "-k", linewidth=5)
+    plt.errorbar(df["epoch"], df["train_num_masked_pixel"]/1024.0, yerr=[np.zeros(len(df["gutout_std_mean"])), df["gutout_std_mean"]/1024.0], color="grey")
+    plt.plot(df["epoch"], df["train_num_masked_pixel"]/1024.0, color="blue", linewidth=5)
 
-    plt.legend(["mean fraction of gutout pixels"])
+    plt.legend(["mean fraction of gutout pixels", "std fraction of gutout pixels"])
     plt.xlabel("epochs")
     plt.ylabel("fraction of gutout pixels")
-    plt.title(f"Mean fraction of gutout pixels w error {experiment_string}")
-    plt.savefig(os.path.join(save_dir, f"pct_gutout_pix_w_error_{experiment_string}.png"))
+    plt.title(f"fraction of gutout pixels - {experiment_string}")
+    plt.savefig(os.path.join(save_dir, f"num_gut_pix{experiment_string}.png"))
+    plt.close()
 
 
 def generate_pct_gutout_pixels_n_gradamp_plot(df, experiment_string, save_dir):
@@ -70,15 +77,17 @@ def generate_pct_gutout_pixels_n_gradamp_plot(df, experiment_string, save_dir):
     lns1 = ax1.plot(df["epoch"], df["train_num_masked_pixel"]/1024.0, "b")
     lns2 = ax2.plot(df["epoch"], df["gradamp_mean_mean"], "g")
 
-    plt.xlabel("epochs")
-    ax1.set_ylim(0.0, 1.0)
+    # ax1.set_ylim(0.0, 1.0)
     ax2.set_ylim(0.0, 1.0)
     ax1.set_ylabel("fraction of gutout pixels")
     ax2.set_ylabel("gradcam amplitude")
+    plt.xlabel("epochs")
+    ax1.set_xlabel("epochs")
 
     lns = lns1 + lns2
     legends = ["fraction of gutout pixels", "gradcam amplitude"]
     ax1.legend(lns, legends)
 
-    plt.title(f"Fraction of gutout pixels & avg gradcam amp {experiment_string}")
-    plt.savefig(os.path.join(save_dir, f"pct_gut_pix_n_avg_gradamp_{experiment_string}.png"))
+    plt.title(f"Fraction of gutout pixels & amplitude of gradcam - {experiment_string}")
+    plt.savefig(os.path.join(save_dir, f"num_gut_pix_and_grad_amp_{experiment_string}.png"))
+    plt.close()
